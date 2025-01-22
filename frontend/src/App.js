4cd39d9ca58a5604b82a4e2d1131e02f634a8a80
Kyle Savage <kylesavage@Kyles-MacBook-Air.local>
2025-01-22 10:19:12 -0500

    Installed react-router / set up job board route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import JobBoard from './scenes/JobBoard/JobBoard';
 import Landing from './scenes/Landing/Landing';
 
 function App() {
   return (
-    <div className="App">
-      <Landing />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route exact path="/my-jobs" element={<JobBoard />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/scenes/JobBoard/JobBoard.js
@@ -0,0 +1,9 @@
+import "./JobBoard.css"
+
+const JobBoard = () => {
+  return (
+    <div>JobBoard</div>
+  )
+}
+
+export default JobBoard
