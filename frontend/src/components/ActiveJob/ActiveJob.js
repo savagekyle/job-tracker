5f6be6f17438fab1bd937ece80a279bb3c1c824a
Kyle Savage <savagerkyle@gmail.com>
2025-02-13 09:25:20 -0500

    view listing button made more visible and repositioned

--- frontend/src/components/ActiveJob/ActiveJob.js
+++ frontend/src/components/ActiveJob/ActiveJob.js
@@ -4,11 +4,14 @@ import DOMPurify from 'dompurify';
 const ActiveJob = ({job}) => {
   return (
     <div className="application-expanded">
+      <div className="active-top">
         <p className="company">{job.company}</p>
         <h2 className="job-title">{job.jobTitle}</h2>
         <p className="location">{job.location}</p>
         <p className="pay">{job.pay ? job.pay : <i>"No compensation provided"</i>}</p>
         <a href={job.url} target="_blank" rel="noreferrer">View Listing</a>
+      </div>
+        
         <hr />
         <div className="job-desc">
         <p dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(job.description) }}></p>
